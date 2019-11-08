0a1b62e4c089dfdc6158ad566df1cb2b9c6e041c
James <james.dodds@me.com>
2019-11-08 14:30:41 +1100

    Update salaryseeker.ps1
    Resolved issue with multiple search returns in loop. Displays salary reported on advert is present.

--- salaryseeker.ps1
+++ salaryseeker.ps1
@@ -1,4 +1,4 @@
-﻿### seek salary seeker
+### seek salary seeker
 ### rewritten from https://github.com/b3n-j4m1n/salary-seeker
 
 Param (
@@ -7,10 +7,10 @@ Param (
 )
 
 ### initial variables
-$counter = 1
+$counter = 0
 $response = 1
-$upper_lim = 2000000
-$lower_lim = 30000
+$upper_lim = 250000
+$lower_lim = 20000
 $salary_var = ($lower_lim + (($upper_lim - $lower_lim)/2))
 $job_data=(ConvertFrom-Json(Invoke-WebRequest -UseBasicParsing -Uri "https://chalice-search-api.cloud.seek.com.au/search?jobid=$($job_id)")).data
 
@@ -23,42 +23,46 @@ Write-Host $job_data.teaser
 Write-Host "Job ID: $($job_data.id)"
 ### find maximum
 while ($counter -lt 16) {
-    $response=(ConvertFrom-Json(Invoke-WebRequest -UseBasicParsing -Uri "https://chalice-search-api.cloud.seek.com.au/search?keywords=$($job_data.teaser)&advertiserid=$($job_data.advertiser.id)&sourcesystem=houston&salaryrange=$($salary_var)-$($upper_lim)"))
-    if($response.totalCount -eq 1) {
+    $response=(ConvertFrom-Json(Invoke-WebRequest -UseBasicParsing -Uri "https://chalice-search-api.cloud.seek.com.au/search?keywords=$($job_data.teaser)&advertiserid=$($job_data.advertiser.id)&sourcesystem=houston&salaryrange=$([math]::round($salary_var,0))-$([math]::round($upper_lim,0))")).data | where {$_.id -eq $job_id}
+    if(($response|measure-object).Count -eq 1) {
         $lower_lim=$salary_var
-        $salary_var=($salary_var + (($upper_lim - $salary_var) / 2)) 
-        #Write-Host "found, upper limit: $($upper_lim), lower limit: $($salary_var)"
+        $salary_var=($salary_var + (($upper_lim - $salary_var) / 2))
+        #Write-host "MAX found 1, new upper: $($upper_lim), new lower: $($salary_var)" 
         }
-    elseif($response.TotalCount -eq 0) {
+    elseif(($response|measure-object).Count -eq 0) {
         $upper_lim=$salary_var
         $salary_var=($salary_var - (($salary_var - $lower_lim) / 2))
-        #write-host "none found, new upper: $($upper_lim), new lower: $($salary_var)"
+        #write-host "MAX found 0, new upper: $($upper_lim), new lower: $($salary_var)" 
         }
     $counter ++
+    #Write-Host "MAX - Var: $($salary_var), lower: $($lower_lim), upper: $($upper_lim)"
 }
-$salary_max = $salary_var
 
+$salary_max = $salary_var
+Write-verbose "BETWEEN CALCS - Var: $($salary_var), lower: $($lower_lim), upper: $($upper_lim)"
 ### variable reset
-$upper_lim = $salary_max
+$counter = 0
 $lower_lim = 25000
+$upper_lim = $salary_max
 $salary_var = ($lower_lim + (($upper_lim - $lower_lim) / 2))
 
-### find maximum
+### find minimum
     while ($counter -lt 16) {
-    $response=(ConvertFrom-Json(Invoke-WebRequest -UseBasicParsing -Uri "https://chalice-search-api.cloud.seek.com.au/search?keywords=$($job_data.teaser)&advertiserid=$($job_data.advertiser.id)&sourcesystem=houston&salaryrange=$($lower_lim)-$($salary_var)"))
-    if($response.totalCount -eq 1) {
+    $response=(ConvertFrom-Json(Invoke-WebRequest -UseBasicParsing -Uri "https://chalice-search-api.cloud.seek.com.au/search?keywords=$($job_data.teaser)&advertiserid=$($job_data.advertiser.id)&sourcesystem=houston&salaryrange=$([math]::round($lower_lim,0))-$([math]::round($salary_var,0))")).data | where {$_.id -eq $job_id}
+    if(($response|measure-object).Count -eq 1) {
         $upper_lim=$salary_var
-        #printf line
         $salary_var=($salary_var - (($salary_var - $lower_lim) / 2)) 
+        #write-host "MIN found 1, new upper: $([math]::round($salary_var,2)), new lower: $([math]::round($lower_lim,2))" 
         }
-    elseif($response.TotalCount -eq 0) {
-        $upper_lim=$salary_var
-        #printf line
+    elseif(($response|measure-object).Count -eq 0) {
+        $lower_lim=$salary_var
         $salary_var=($salary_var + (($upper_lim - $salary_var) / 2))
+        #write-host "MIN found 0, new upper: $([math]::round($salary_var,2)), new lower: $([math]::round($lower_lim,2))"
         }
     $counter ++
 } 
 $salary_min = $salary_var
 
 Write-Host "-----------------"
-Write-Host "Minimum salary: $([math]::round($salary_min,2)) | Maximum salary: $([math]::Round($salary_max,2))"
+Write-Host -NoNewline "Minimum salary: `$$([math]::round($salary_min,2)) | Maximum salary: `$$([math]::Round($salary_max,2))"
+if ($job_data.salary -ne ""){Write-Host -nonewline "`r | Reported Range: $($job_data.salary)" }
